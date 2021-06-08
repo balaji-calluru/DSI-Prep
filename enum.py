fb9f35007e197ba5d982121a442bdbeeb6f477f7
Balaji Calluru <balaji.calluru@gmail.com>
2021-06-07 20:20:56 -0500

    Initial Commit

---
+++ enum.py
@@ -0,0 +1,11 @@
+names = ["Balaji", "Vasanthi", "Surya"]
+ages  = [47,       40,         14]
+
+for name, age in zip(names, ages):
+    print(f'{name} is {age} years old')
+
+lst_a = [4, 3, 7, 6, 5, 9, 2, 4]
+
+for indx, a in enumerate(lst_a):
+    print(indx, a)
+
