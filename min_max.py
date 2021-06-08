0e1e81145f05ab653f9f5309ab0e81fa9abaf467
Balaji Calluru <balaji.calluru@gmail.com>
2021-06-08 08:32:49 -0500

    Good progress

--- min_max.py
+++ min_max.py
@@ -4,3 +4,9 @@ print(min(lst_a))
 lst_a = ["Balaji", "Vasanthi", "Surya"]
 print(max(lst_a))
 print(min(lst_a))
+
+## Too many values to unpack
+#a, b, c = [1,2,3,4]
+
+## not enough values to unpack
+#a, b, c = [1,2]
