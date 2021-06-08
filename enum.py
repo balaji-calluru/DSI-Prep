8d462958ff3cf9c2a4c014ef16ce856e248c0d14
Balaji Calluru <balaji.calluru@gmail.com>
2021-06-07 20:25:11 -0500

    Updated Enum

--- enum.py
+++ enum.py
@@ -1,11 +1,18 @@
 names = ["Balaji", "Vasanthi", "Surya"]
-ages  = [47,       40,         14]
+ages  = [46,       39,         14]
 
 for name, age in zip(names, ages):
     print(f'{name} is {age} years old')
 
 lst_a = [4, 3, 7, 6, 5, 9, 2, 4]
+lst_b = [1, 4, 6, 7, 4, 6]
 
 for indx, a in enumerate(lst_a):
     print(indx, a)
 
+acc = 0
+for i, a, b in zip(range(len(lst_a)), lst_a, lst_b):
+    print(i, a, b)
+    acc += (a / b) ** i
+
+print(acc)
