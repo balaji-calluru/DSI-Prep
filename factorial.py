e88c333ee53896afa957625d3d8a301adc9ef846
Balaji Calluru <balaji.calluru@gmail.com>
2021-06-17 22:10:50 -0500

    Just WOW!!!

--- factorial.py
+++ factorial.py
@@ -91,5 +91,101 @@ five_perms = heaps_non_recursive(base_5, 5)
 
 def combinations(n, k):
     return(factorial(n) // (factorial(n-k) * factorial(k)))
-print(combinations(5,3))
 
+def combinations2(n, k):
+    perm = 1
+    for i in range(n, n-k, -1):
+        perm *= i
+    return int(perm / factorial(k))
+
+# print(combinations(5,3))
+# print(combinations2(5,3))
+
+# print(list(range(5,1,-1)))
+
+# an expensive counting approach
+'''
+Out of 11 basketball players, we can have teams
+of 5. How many teams of 5 are possible?
+'''
+
+num_comb = combinations2(11, 5)
+
+def combinations3():
+    eleven_nums = range(1, 11+1)
+    possible_five = []
+
+    for i in eleven_nums:
+        for j in eleven_nums:
+            for k in eleven_nums:
+                for l in eleven_nums:
+                    for m in eleven_nums:
+                        possible_five.append([i,j,k,l,m])
+    
+    perms = []
+    for five in possible_five:
+        if len(list(set(five))) == 5:
+            perms.append(five)
+
+#    for five in perms:
+#        print(five)
+
+# combinations3()
+
+# an expensive sampling approach
+from random import choice
+
+def basketball_combs_samp(team_size=11, num_players=5):
+    comb_count = combinations(team_size, num_players)
+    comb_index = 0
+    combs = []
+    player_range = range(1, team_size+1)
+    while len(combs) < combinations(team_size, num_players):
+        player_comb = []
+        while len(player_comb) < num_players:
+            player_num = choice(player_range)
+            if player_num not in player_comb:
+                player_comb.append(player_num)
+        player_comb = sorted(player_comb)
+        if player_comb not in combs:
+            comb_index += 1
+            print(comb_index, comb_count, player_comb)
+            combs.append(player_comb)
+    return combs
+team_size = 21
+num_players = 5
+#print(len(basketball_combs_samp(team_size , num_players)))
+#print()
+
+def combs_from_itertools(lst, k):
+    # get a frozen version of the input
+    lst_frozen = tuple(lst)
+    n = len(lst_frozen)
+    combs = []
+    if k > n: return
+    indices = list(range(k))
+    yield tuple(lst_frozen[i] for i in indices)
+    while True:
+        for i in reversed(range(k)):
+            if indices[i] != i + n - k:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, k):
+            indices[j] = indices[j-1] + 1
+        yield tuple(lst_frozen[i] for i in indices)
+bask_nums = list(range(1, 21+1))
+counter = 0
+#for team in combs_from_itertools(bask_nums, 5):
+#    print(team)
+#    counter += 1
+#print(counter)
+#print(combinations(21, 5))
+
+print(list(reversed(list(range(10)))))
+print(list(range(9,-1,-1)))
+
+lst = [1,1,2,2,3,3,4,4,5,5]
+st = set(lst)
+print(st)
